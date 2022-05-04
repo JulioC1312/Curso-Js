2d63ea306d9a8468eefe8761a492d3ade3633d04
JulioC1312 <juliocesar2024.jc@gmail.com>
2022-05-04 15:31:52 -0300

    aulas e exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel} Km/h `)
+if(vel > 60){//condição simples
+    console.log('você ultrapassou o limite de velocidade,MULTADO')
+}
+console.log('dirija sempre usando cinto de segurança')
