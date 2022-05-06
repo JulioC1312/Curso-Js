62a775602cfb4d5cfe983e3b5d92d4e27859564b
JulioC1312 <juliocesar2024.jc@gmail.com>
2022-05-06 17:09:27 -0300

    modificaçoes

--- exercicios/ex01 - Copia/script.js
+++ exercicios/ex01 - Copia/script.js
@@ -3,8 +3,8 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours() 
-    
-    msg.innerHTML = `Agora sao ${hora} hora`
+   
+     msg.innerHTML = `Agora sao ${hora} hora`
     if(hora >= 0 && hora < 12){
         //bom dia
         img.src = 'manha.png'
