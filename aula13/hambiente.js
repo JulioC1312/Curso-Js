627db1778a6e4060b3c567e01468dd0fd550cc81
JulioC1312 <juliocesar2024.jc@gmail.com>
2022-05-10 17:33:14 -0300

    exercicios

---
+++ aula13/hambiente.js
@@ -0,0 +1,37 @@
+var c = 1
+while (c <= 10) {
+    console.log(`voce é o numero ${c}`)
+    c++
+}
+
+
+
+
+/*
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++ //recebe c = c + 1
+} while(c <= 6)
+*/
+
+
+
+/* testa primeiro e execulta depois
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++ //recebe c = c + 1
+}
+*/
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
